9fccf1b0050d22b0e480653efe8dcb33f73238ad
Samuel DAUZON <git@dauzon.com>
2017-04-13 23:20:31 +0200

    Rechercher tous les documents

---
+++ client/main.js
@@ -0,0 +1,27 @@
+import { Template } from 'meteor/templating';
+
+Meteor.startup(() => {
+    ProduitsCollection.insert({
+        nom: 'Sac de blé - 25 Kg',
+        prix: 12,
+        reference: '0738483926',
+        quantiteStock:8
+    });
+    ProduitsCollection.insert({
+        nom: 'Mélange de graines - 50 Kg',
+        prix: 29,
+        reference: '0794762842',
+        quantiteStock:3
+    });
+    ProduitsCollection.insert({
+        nom: 'Poulailler 6 poules',
+        prix: 199,
+        reference: '0798658495',
+        quantiteStock:0
+    });
+
+    curseur = ProduitsCollection.find();
+    curseur.forEach(function(document){
+      console.log(document);
+    });
+});
