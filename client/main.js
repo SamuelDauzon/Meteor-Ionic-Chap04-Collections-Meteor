83bd8d82536f51ef45eda070f8d1175e600685dd
Samuel DAUZON <git@dauzon.com>
2017-04-20 23:54:13 +0200

    Rechercher avec un sélecteur

--- client/main.js
+++ client/main.js
@@ -5,21 +5,27 @@ Meteor.startup(() => {
         nom: 'Sac de blé - 25 Kg',
         prix: 12,
         reference: '0738483926',
+        categorie: 'Céréales',
         quantiteStock:8
     });
     ProduitsCollection.insert({
         nom: 'Mélange de graines - 50 Kg',
         prix: 29,
         reference: '0794762842',
+        categorie: 'Céréales',
         quantiteStock:3
     });
     ProduitsCollection.insert({
         nom: 'Poulailler 6 poules',
         prix: 199,
         reference: '0798658495',
+        categorie: 'Poulailler',
         quantiteStock:0
     });
 
-    produit = ProduitsCollection.findOne({reference: '0794762842'});
-    console.log(produit);
+    curseur = ProduitsCollection.find({ categorie: 'Céréales'});
+    curseur.forEach(function(document){
+      console.log(document);
+    });
+
 });
