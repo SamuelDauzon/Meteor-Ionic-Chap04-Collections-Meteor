2c755169a20f07b18e0d67876678a45e53ba371d
Samuel DAUZON <git@dauzon.com>
2017-04-21 00:01:49 +0200

    Tri de recherche

--- client/main.js
+++ client/main.js
@@ -5,25 +5,22 @@ Meteor.startup(() => {
         nom: 'Sac de blé - 25 Kg',
         prix: 12,
         reference: '0738483926',
-        categorie: 'Céréales',
         quantiteStock:8
     });
     ProduitsCollection.insert({
         nom: 'Mélange de graines - 50 Kg',
         prix: 29,
         reference: '0794762842',
-        categorie: 'Céréales',
         quantiteStock:3
     });
     ProduitsCollection.insert({
         nom: 'Poulailler 6 poules',
         prix: 199,
         reference: '0798658495',
-        categorie: 'Poulailler',
         quantiteStock:0
     });
 
-    curseur = ProduitsCollection.find({ categorie: 'Céréales'});
+    curseur = ProduitsCollection.find({}, {sort: [["prix", "desc"], ["quantiteStock", "asc"]]});
     curseur.forEach(function(document){
       console.log(document);
     });
