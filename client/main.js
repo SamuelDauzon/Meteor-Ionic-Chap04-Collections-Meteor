775a9a7d08fd697ebb13da163be5b748390660b0
Samuel DAUZON <git@dauzon.com>
2017-04-23 02:52:51 +0200

    Exclusion de champs

--- client/main.js
+++ client/main.js
@@ -20,7 +20,7 @@ Meteor.startup(() => {
         quantiteStock:0
     });
 
-    curseur = ProduitsCollection.find({}, {limit: 2, sort: {prix: "desc"}});
+    curseur = ProduitsCollection.find({}, {fields: {prix: 0, quantiteStock: 0}});
     curseur.forEach(function(document){
       console.log(document);
     });
